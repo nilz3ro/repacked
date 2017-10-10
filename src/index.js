4eff3dad7e72bdf862b7e2bc41a352b0bf3e6c7f
Austin Buddington <austin.j.buddington@gmail.com>
2017-10-10 14:38:36 -0400

    MVP.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
   render () {
-    return (<h1>Testing JSX.</h1>);
+    return (
+      <div>
+        <h1>Testing JSX.</h1>
+        <Ticker />
+      </div>
+    );
   }
 }
+
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      iterations: 0
+    };
+  }
+
+  render () {
+    console.log('test')
+    this.tick();
+    return (<span>This has been rendered {this.state.iterations} times.</span>)
+  }
+
+  tick () {
+    setTimeout(() => {
+      this.setState({iterations: this.state.iterations + 1});
+    }, 1000)
+  }
+}
+
 
 ReactDOM.render(<App/>, document.getElementById('application_root'));
